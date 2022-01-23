def7be699125e3984d875a8b80f8968779163df4
bruna <brunabutten@gmail.com>
2022-01-23 16:42:46 -0300

    Requisito 04

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,14 +23,19 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+/* REQUISITO 04 */
+
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('x')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(typeof(circle(10))).toBe('object');
     // Teste se o objeto retornado tem 3 propriedades.
+    expect(Object.values(circle(5)).length).toEqual(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
