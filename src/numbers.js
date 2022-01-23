e4c48da88e2a6bcf7911579659cdb459fbfbb174
bruna <brunabutten@gmail.com>
2022-01-23 16:28:06 -0300

    Requisito 01

--- src/numbers.js
+++ src/numbers.js
@@ -9,6 +9,8 @@
     - numbers([2, 'errado', 5]); // Retorna: false
 */
 
+/* REQUISITO 01 */
+
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
